031a5974f574d64047dec7562f0a695afec21e57
wineguo <wineguo@tencent.com>
2025-02-04 08:36:20 +0800

    scale it

--- v2.py
+++ v2.py
@@ -5,11 +5,11 @@ from torch.utils.tensorboard import SummaryWriter
 import shutil
 import os
 
-batch_size = 32  # how many independent sequences will we process in parallel?
-block_size = 8  # what is the maximum context length for predictions?
+batch_size = 64  # how many independent sequences will we process in parallel?
+block_size = 256  # what is the maximum context length for predictions?
 max_iters = 5000
-eval_interval = 500
-learning_rate = 1e-3
+eval_interval = 1
+learning_rate = 3e-4
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -18,7 +18,10 @@ device = (
 device = 'cpu'
 print(f"Using device: {device}")
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layers = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -86,6 +89,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         # (B, T, C), B: batch size, T: sequence length, C: embedding dimension
@@ -95,6 +99,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * self.head_size**-0.5  # (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))  # (B,T,T)
         wei = F.softmax(wei, dim=-1)  # (B,T,T)
+        wei = self.dropout(wei)
         v = self.value(x)  # (B, T, head_size)
         out = wei @ v  # (B, T, head_size)
         return out
@@ -106,10 +111,12 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         out = torch.cat([head(x) for head in self.heads], dim=-1)
         out = self.proj(out)
+        out = self.dropout(out)
         return out
 
 class FeedForward(nn.Module):
@@ -120,6 +127,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, n_embd*4),
             nn.ReLU(),
             nn.Linear(n_embd*4, n_embd),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -145,12 +153,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd, device=device)
         self.position_embedding_table = nn.Embedding(block_size, n_embd, device=device)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            nn.LayerNorm(n_embd),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layers)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size, device=device)
 
     def forward(self, idx, targets=None):
@@ -159,6 +163,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
         if targets is None:
             loss = None
